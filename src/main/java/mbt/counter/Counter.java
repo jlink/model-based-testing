c69eb0130e04d3c922debcbc5bddb3c8690088ed
Johannes Link <business@johanneslink.net>
2020-02-19 13:15:58 +0100

    Added Counter implementation

--- src/main/java/mbt/counter/Counter.java
+++ src/main/java/mbt/counter/Counter.java
@@ -1,4 +1,22 @@
 package mbt.counter;
 
 public class Counter {
+
+	private int value = 0;
+
+	public void inc() {
+		value++;
+	}
+
+	public void set(int value) {
+		this.value = value;
+	}
+
+	public int value() {
+		return value;
+	}
+
+	public void clear() {
+		value = 0;
+	}
 }
